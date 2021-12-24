903fff9c70b62443986399396f2ea1919a961b00
Rafidaone <84330533+Rafidaone@users.noreply.github.com>
2021-12-23 22:20:42 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,6 +17,7 @@ sudo apt install hashcat -y
 
 rm tools.py
 wget https://raw.githubusercontent.com/Rafidaone/KaliTools/main/tools.py
-
+echo "---------------------------"
+echo "finished task now exiting"
 exit
 
