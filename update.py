b193c2284aed04d7e903968915317f910c707de0
Rafidaone <84330533+Rafidaone@users.noreply.github.com>
2021-12-23 22:25:49 -0600

    Update update.py

--- update.py
+++ update.py
@@ -19,5 +19,23 @@ with open("tools.py", 'wb') as f:
     f.write(r.content)
 print("finished downloading tools.py")
 print(Fore.GREEN + "you now have the newest version of Kali Tools!")
+
+
+
+print(Fore.LIGHTBLUE_EX + "installing update.sh")
+#this is to cheak if there is a file tools.py so it wont error out if there is no spam-bot.py file in the folder
+if os.path.isfile('update.sh'):
+    print("deleting update.sh")
+    os.remove("update.sh")
+    time.sleep(0.5)
+    print("deleted update.sh")
+
+image_url = "https://raw.githubusercontent.com/Rafidaone/KaliTools/main/update.sh"
+print("downloading new update script from " + image_url)
+r = requests.get(image_url)
+with open("tools.py", 'wb') as f:
+    f.write(r.content)
+print("finished downloading update.sh")
+print(Fore.GREEN + "update.py is now discatinued by the developer in exchange for update.sh")
 time.sleep(1.5)
 input('press enter to exit')
