027ff6295430b0c41dd5557adf96a54193da5c82
Rafidaone <84330533+Rafidaone@users.noreply.github.com>
2021-12-29 21:18:34 -0600

    Update update.py

--- update.py
+++ update.py
@@ -2,21 +2,25 @@ import os
 import requests
 from colorama import Fore
 import time
+print("if you want to update please run the update coommand in tools.py")
 
 
-print(Fore.LIGHTBLUE_EX + "updating tools.py")
+def update():
+    print(Fore.LIGHTBLUE_EX + "updating tools.py")
 #this is to cheak if there is a file tools.py so it wont error out if there is no spam-bot.py file in the folder
-if os.path.isfile('tools.py'):
-    print("deleting tools.py")
-    os.remove("tools.py")
-    time.sleep(0.5)
-    print("deleted tools.py")
-#this is were it connects to my web site to get the newes vershion of spam-bot
-image_url = "https://raw.githubusercontent.com/Rafidaone/KaliTools/main/tools.py"
-print("downloading kali tools from " + image_url)
-r = requests.get(image_url)
-with open("tools.py", 'wb') as f:
-    f.write(r.content)
-print("finished downloading tools.py")
-print(Fore.GREEN + "you now have the newest version of Kali Tools!")
-input("press enter to exit")
+    if os.path.isfile('tools.py'):
+        print("deleting tools.py")
+        os.remove("tools.py")
+        print("deleted tools.py")
+#this is were it connects to my github to get the newes vershion of kalitools
+    image_url = "https://raw.githubusercontent.com/Rafidaone/KaliTools/main/tools.py"
+    print("downloading kali tools from " + image_url)
+    r = requests.get(image_url)
+    with open("tools.py", 'wb') as f:
+        f.write(r.content)
+    print("finished downloading tools.py")
+    print(Fore.GREEN + "you now have the newest version of Kali Tools!")
+    input("press enter to exit")
+
+
+update()
